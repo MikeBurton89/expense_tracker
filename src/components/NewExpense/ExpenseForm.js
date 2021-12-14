9228241d5a81d065c3d1161b65de5d6cb3e90437
Michele Puricelli <michele.puricelli89@gmail.com>
2021-12-14 14:20:52 +0100

    added conditionals

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -58,6 +58,7 @@ const ExpenseForm = (props) => {
         setEnteredAmount('')
     };
 
+
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
@@ -75,7 +76,8 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__action'>
-                <button type='submit'> Add Expense</button>
+                <button type='button' onClick={props.onCancel}> Cancel </button>
+                <button type='submit'> Add Expense </button>
             </div>
         </form >
     )
